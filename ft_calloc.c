29f9883be2c8e69f06a9e9775c748d06799abc62
Josias Kasongo <josiaskas@gmail.com>
2021-05-13 16:53:24 -0400

    correcting errors on calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jkasongo <jkasongo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 16:17:31 by jkasongo          #+#    #+#             */
-/*   Updated: 2021/05/13 16:33:48 by jkasongo         ###   ########.fr       */
+/*   Updated: 2021/05/13 16:53:01 by jkasongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*heap_p;
 
-	if ((size == 0) || (count == 0))
-		return (0);
 	heap_p = malloc(count * size);
 	if (!heap_p)
 		return (0);
-	ft_bzero(heap_p, count);
+	ft_bzero(heap_p, (count * size));
 	return (heap_p);
 }
