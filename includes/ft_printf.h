e539d6bd9a1f017dfd44e8ffa3086f8be74b8d14
Josias Kasongo <jkasongo@c2r6p10.42quebec.com>
2021-05-23 14:27:14 -0400

    arg typedef

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jkasongo <jkasongo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 13:18:08 by jkasongo          #+#    #+#             */
-/*   Updated: 2021/05/23 13:18:21 by jkasongo         ###   ########.fr       */
+/*   Updated: 2021/05/23 14:25:26 by jkasongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@
 # include <sys/uio.h>
 # include <stdarg.h>
 # include "../libft/libft.h"
+
+typedef struct s_arg
+{
+	char	*format;
+	int		position;
+	char	flags;
+	int		width;
+	int		precison;
+	char	specifier;
+}	t_arg;
+
+typedef int	(*t_parse_arg)(t_arg);
 
 int	ft_printf(const char *s, ...);
 
