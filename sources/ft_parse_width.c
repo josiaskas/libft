efb72db92fa4013433c66f1acdd1f150268c79d4
Josias Kasongo <jkasongo@c2r6p10.42quebec.com>
2021-05-27 13:24:08 -0400

    correcting width with zero inside

--- sources/ft_parse_width.c
+++ sources/ft_parse_width.c
@@ -6,11 +6,27 @@
 /*   By: jkasongo <jkasongo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 13:54:23 by jkasongo          #+#    #+#             */
-/*   Updated: 2021/05/25 13:17:34 by jkasongo         ###   ########.fr       */
+/*   Updated: 2021/05/27 13:21:15 by jkasongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+static void	mini_itoa_width(t_arg *arg, char c)
+{
+	int	width;
+
+	width = 0;
+	while ((c >= '0') && (c <= '9'))
+	{
+		width = width * 10;
+		width += (int)(c - 48);
+		arg->part[1] = 1;
+		arg->min_width = width;
+		arg->cursor++;
+		c = arg->format[arg->cursor];
+	}
+}
 
 //[1-9 (only if flag specified first, not true) or * (value inside the args)]
 void	ft_parse_width(t_arg *arg)
@@ -28,14 +44,7 @@ void	ft_parse_width(t_arg *arg)
 		arg->cursor++;
 		return ;
 	}
-	while ((c >= '1') && (c <= '9'))
-	{
-		width = width * 10;
-		width += (int)(c - 48);
-		arg->part[1] = 1;
-		arg->min_width = width;
-		arg->cursor++;
-		c = arg->format[arg->cursor];
-	}
+	if ((c >= '1') && (c <= '9'))
+		mini_itoa_width(arg, c);
 	return ;
 }
---
+++ test.c
@@ -0,0 +1,9 @@
+#include "includes/ft_printf.h"
+#include <stdio.h>
+
+int main(void)
+{
+	ft_printf("%10c",'0');
+	printf("%10c",'0');
+	return (0);
+}
