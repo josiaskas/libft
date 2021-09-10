cf33226da416f6e3cb91a0ee9c0ca30f036de22b
Josias Kasongo <josiaskas@gmail.com>
2021-09-10 10:01:11 -0400

    norm error

--- arrays/ft_slice.c
+++ arrays/ft_slice.c
@@ -6,42 +6,48 @@
 /*   By: jkasongo <jkasongo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 22:28:59 by jkasongo          #+#    #+#             */
-/*   Updated: 2021/08/30 10:10:18 by jkasongo         ###   ########.fr       */
+/*   Updated: 2021/09/10 10:00:56 by jkasongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/array_utils.h"
 
-int	*ft_slice_int(const int original[], int begin, int end)
+static int	*run_copy(int begin, int end, const int original[])
 {
-	unsigned int	size;
 	int				i;
 	int				j;
 	int				*copy;
 
-	size = end - begin;
 	i = 0;
 	j = 0;
-	copy = NULL;
-	if (size > 0)
+	copy = (int *)malloc(sizeof(int) * (end - begin));
+	if (!copy)
+		return (NULL);
+	if (original)
 	{
-		copy = (int *)malloc(sizeof(int) * size);
-		if (!copy)
-			return (NULL);
-		if (original)
+		while (original[i])
 		{
-			while (original[i])
+			if ((i >= begin) && (i < end))
 			{
-				if ((i >= begin) && (i < end))
-				{
-					copy[j] = original[i];
-					j++;
-				}
-				if (i == end)
-					break ;
-				i++;
+				copy[j] = original[i];
+				j++;
 			}
+			if (i == end)
+				break ;
+			i++;
 		}
 	}
+	return (copy);
+}
+
+int	*ft_slice_int(const int original[], int begin, int end)
+{
+	unsigned int	size;
+	int				*copy;
+
+	size = end - begin;
+	copy = NULL;
+	if (size > 0)
+		copy = run_copy(begin, end, original);
 	return (copy);
 }
