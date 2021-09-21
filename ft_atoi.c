5986f885b3fbeb3b511ed9a1b679502e84196d1a
Josias Kasongo <josiaskas@gmail.com>
2021-09-21 16:48:23 -0400

    norm error

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jkasongo <jkasongo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 03:54:47 by jkasongo          #+#    #+#             */
-/*   Updated: 2021/05/11 14:23:44 by jkasongo         ###   ########.fr       */
+/*   Updated: 2021/09/21 16:47:38 by jkasongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 static int	ft_isspace_local(int c)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r')
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r')
 		return (1);
 	else
 		return (0);
